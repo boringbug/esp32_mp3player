6f9d77331d695311bfe80d8f44d227cc58e8492a
boringbug <paradiznik5@gmail.com>
2026-06-06 22:32:53 +0200

    first commit

---
+++ mp3player_software/main/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+int nth_fibb(int n) {
+	int sum = 0;
+
+	for(int i = 0; i <= n; i++) {
+		sum = sum + i;
+	}
+
+	return sum;
+}
+
+void preaty_print(int n) {
+	std::cout << n;
+	std::cout << "-th sum is: ";
+	std::cout << nth_fibb(n) << '\n';
+}
+
+void hello_world() {
+    std::cout << "====Starting====" << "\n";
+    std::cout << "Hello World from C++!" << "\n";
+}
+
+extern "C" void app_main() {
+	hello_world();
+    
+	int n = 10;
+
+	for(int j = 0; j <= n; j++) {
+		preaty_print(j);
+	}
+
+    while(1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));  // Wait 1 second
+    }
+}
